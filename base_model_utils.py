a38dd8560dc604dcc5e1095ccba7e0ffa668cb3a
SteeMod <stephenmodimakwane@gmail.com>
2024-08-13 07:20:30 +0000

    s

--- base_model_utils.py
+++ base_model_utils.py
@@ -5,7 +5,7 @@ import re
 
 def call_chat_model(client, messages):
     return client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-3.5",
         messages=messages,
         temperature=1,
         max_tokens=4096
