547929aafae051fbb800cde13b101290c78590ce
SteeMod <stephenmodimakwane@gmail.com>
2024-08-15 21:16:26 +0000

    ser

--- pages/3_Nutrition_Agent_[RAG].py
+++ pages/3_Nutrition_Agent_[RAG].py
@@ -10,7 +10,7 @@ def check_api_key():
     if not api_key:
         st.error("API key not found. Please set the OPENAI_API_KEY environment variable.")
         st.stop()
-    openai.api_key = api_key
+    openai.api_key = api_key 
 
 def initialize_session_state():
     if "nutrition_response" not in st.session_state:
@@ -25,16 +25,16 @@ def display_header():
     st.subheader("Give your preferred treatment services, e.g medication treatment, Helplines, Behavioral Therapy")
 
 def get_user_input():
-    return st.text_input(label="nutrition_agent", label_visibility="hidden", placeholder="What are some Mediterranean breakfast options?")
+    return st.text_input(label="nutrition_agent", label_visibility="hidden", placeholder="What are some preffered help options?")
 
 def request_nutrition_options(user_input):
     try:
-        prompt = f"Provide me a list of some {user_input} MOUD support service programs"
+        prompt = f"Provide me a list of some {user_input} OUD support service programs"
         
         response = openai.ChatCompletion.create(
             model="gpt-4",
             messages=[
-                {"role": "system", "content": "You are a helpful assistant. You have to work solely on user input specific to only what the user says. Do not include MOUD in your output. Include contacts and links in your output and only be very specific based on exact user output. For example, if a user asks for medication treatment, don't give therapy treatment."},
+                {"role": "system", "content": "You are a helpful assistant. You have to work solely on user input specific to only what the user says. Do not include MOUD in your output. Include  contacts and links in your output, also take text from those sites and only be very specific based on exact user output. For example, if a user asks for medication treatment, don't give therapy treatment."},
                 {"role": "user", "content": prompt}
             ],
             max_tokens=150
