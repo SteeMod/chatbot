8b41f59b1cc87e83fd3dbd3eed305233b0c4ed62
SteeMod <stephenmodimakwane@gmail.com>
2024-08-19 02:42:44 +0000

    Praise the LORD my GOD

--- pages/5_test.py
+++ pages/5_test.py
@@ -3,15 +3,17 @@ import streamlit as st
 # Title of the app
 st.title("Comment Section")
 
+# Initialize session state for comments
+if "comments" not in st.session_state:
+    st.session_state["comments"] = []
+
 # Function to display comments
-@st.fragment
 def display_comments():
     comments = st.session_state.get("comments", [])
     for comment in comments:
         st.write(comment)
 
 # Function to add a new comment
-@st.fragment
 def add_comment():
     with st.form(key="comment_form"):
         new_comment = st.text_area("Add your comment")
