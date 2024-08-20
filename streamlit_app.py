1a1b1368e52f22ca75add0a795183414d97b121a
SteeMod <stephenmodimakwane@gmail.com>
2024-08-20 23:48:39 +0000

    +====================================================================----

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import os
 import streamlit as st
 from azure.storage.blob import BlobServiceClient
-from PIL import Image
-from io import BytesIO
+st.write(" Welcome to MyOFreeApp, login to get started username= MyOFree, password=Imfree123, then you may proceed to other pages")
 
-st.write("Welcome to MyOFreeApp, login to get started username= MyOFree, password=Imfree123, then you may proceed to other pages")
 
 # Retrieve your Azure Blob Storage connection string from an environment variable
 connection_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
@@ -28,16 +26,16 @@ else:
     descriptions = ["Description for Image 1", "Description for Image 2", "Description for Image 3"]
 
     # Retrieve blobs and display images
-    cols = st.columns(len(blob_names))
-    for col, blob_name, description in zip(cols, blob_names, descriptions):
+    for blob_name, description in zip(blob_names, descriptions):
         blob_path = f"{folder_name}/{blob_name}"
         blob_client = blob_service_client.get_blob_client(container_name, blob_path)
         try:
             blob_data = blob_client.download_blob()
-            image = Image.open(BytesIO(blob_data.readall()))
-            image = image.resize((300, 300))  # Resize the image to 300x300 pixels
-            with col:
-                st.image(image, width=300)  # Display the resized image
-                st.write(description)
+            st.image(blob_data.readall(), width=200, caption=blob_name)
+            st.write(description)
         except Exception as e:
             st.error(f"Error downloading {blob_name}: {e}")
+
+
+
+
