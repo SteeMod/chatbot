b6ced2597885ab76fa45a676e6379498b906cf1f
SteeMod <stephenmodimakwane@gmail.com>
2024-08-20 02:57:16 +0000

    GOD is good

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.write(" Welcome to MyOFreeApp, login to get started username= ano, password=8/8/24, then you may proceed to other pages")
+st.write(" Welcome to MyOFreeApp, login to get started username= MyOFree, password=Imfree123, then you may proceed to other pages")
