3cece09f99795ed70ad4d544025500982611fffa
SteeMod <stephenmodimakwane@gmail.com>
2024-08-21 21:54:15 +0000

    t8887

--- image.py
+++ image.py
@@ -2,15 +2,15 @@ import streamlit as st
 from PIL import Image
 
 # Load images from local files
-image1 = Image.open("/workspaces/chatbot/images/image.png")
-image2 = Image.open("/workspaces/chatbot/images/image2.png")
+image1 = Image.open("/workspaces/chatbot/images/image2.png")
+image2 = Image.open("/workspaces/chatbot/images/image.png")
 image3 = Image.open("/workspaces/chatbot/images/image3.png")
 
 # Descriptions of the images
 descriptions = [
-    "Recovery is more effective when individuals come together for treatment. Our focus is on nurturing, educating, and empowering our patients to move towards a brighter future. Let us help you leave this chapter behind and embrace a fresh start.",
-    "Successful treatment involves the entire community's support. At MyOFree, we understand the challenges of addiction and the strength that comes from overcoming it. Join us in your journey towards healing and contact us for more details.",
-    "Your journey to recovery is unique, just like you. Our tailored services are designed to educate and strengthen you, both mentally and physically. We'll walk alongside you, offering support, motivation, and the reassurance that you're never alone.    "
+    "Go to the MYOEvent Finder, this page helps you find in person or virtual events (treatment, awareness, Prevention and Support events) about opiod misuse and overdose",
+    "MyOSupport Service Finder lets you find support services (medication, bejavioral therapy or helplines) based on your preffered treatment service.",
+    ""
 ]
 
 # Display images with descriptions
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import os
 import streamlit as st
 from azure.storage.blob import BlobServiceClient
 
-st.write("Welcome to MyOFreeApp prototype, it is designed to  empower invididuals, to be OFree (Opiod Free) and live fufilling lives by probifinh end to end information on opiod misuse/overdose, the app is still in testing phase so please leave a comment. First to use the app you will need to go to the home page through the sidebar, on mobile it looks like this < login to get started. The username is  myofree, Password: imfree123. Then you may proceed to other pages via the side bar.")
+st.write("Welcome to MyOFreeApp prototype, it is designed to  empower invididuals, to be OFree (Opiod Free) and live fufilling lives by providing end to end information on opiod misuse/overdose, the app is still in testing phase so please leave a comment. First to use the app you will need to go to the home page through the sidebar, on mobile it looks like this < login to get started. The username is  myofree, Password: imfree123. Then you may proceed to other pages via the side bar.")
 
