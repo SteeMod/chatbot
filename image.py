1e5670bf68272ca634c9890c01b13abd6c86e8f0
SteeMod <stephenmodimakwane@gmail.com>
2024-08-21 20:06:11 +0000

    f

--- image.py
+++ image.py
@@ -8,9 +8,9 @@ image3 = Image.open("/workspaces/chatbot/images/image3.png")
 
 # Descriptions of the images
 descriptions = [
-    "Description 1",
-    "Description 2",
-    "Description 3"
+    "Recovery is more effective when individuals come together for treatment. Our focus is on nurturing, educating, and empowering our patients to move towards a brighter future. Let us help you leave this chapter behind and embrace a fresh start.",
+    "Successful treatment involves the entire community's support. At MyOFree, we understand the challenges of addiction and the strength that comes from overcoming it. Join us in your journey towards healing and contact us for more details.",
+    "Your journey to recovery is unique, just like you. Our tailored services are designed to educate and strengthen you, both mentally and physically. We'll walk alongside you, offering support, motivation, and the reassurance that you're never alone.    "
 ]
 
 # Display images with descriptions
