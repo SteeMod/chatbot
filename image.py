49fd4d3d6722d182cd961f7758e8a5b2b10f5c0c
SteeMod <stephenmodimakwane@gmail.com>
2024-08-21 19:02:43 +0000

    !@#$%^&*()

---
+++ image.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image
+
+# Load images from local files
+image1 = Image.open("/workspaces/chatbot/images/image.png")
+image2 = Image.open("/workspaces/chatbot/images/image2.png")
+image3 = Image.open("/workspaces/chatbot/images/image3.png")
+
+# Descriptions of the images
+descriptions = [
+    "Description 1",
+    "Description 2",
+    "Description 3"
+]
+
+# Display images with descriptions
+st.image(image1, caption=descriptions[0], width=500)  # Adjust the width as needed
+st.image(image2, caption=descriptions[1], width=500)  # Adjust the width as needed
+st.image(image3, caption=descriptions[2], width=500)  # Adjust the width as needed
