d46d6a5d8a69f37062c3048c67bbaf002e72676d
SteeMod <stephenmodimakwane@gmail.com>
2024-08-21 23:17:45 +0000

    GOD is good

---
+++ pages/8_Medtracker.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.subheader("Welcome to the Medtracker page")
+st.write("Medication Intake tracker is a pen and paper based recording and tracking app that lets you record medication intake on pen and paper and transforms it to digital data. Please click the link to access the prototype: https://medtracker.streamlit.app/")
