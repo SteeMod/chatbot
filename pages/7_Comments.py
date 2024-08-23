7de9e47ec9fc6d0a97bef068c8d95b0d1d30b8fa
SteeMod <stephenmodimakwane@gmail.com>
2024-08-23 17:55:56 +0000

    Praise GOD

--- pages/7_Comments.py
+++ pages/7_Comments.py
@@ -1,38 +1,3 @@
 import streamlit as st
-import os
 
-# Ensure the comments directory exists
-comments_dir = 'comments'
-if not os.path.exists(comments_dir):
-    os.makedirs(comments_dir)
-
-# Create a form with a text area in Streamlit
-with st.form(key='Comment'):
-    text_input = st.text_area(
-        label='Enter your comment', 
-        value='', 
-        placeholder='Include who the message is from and give detailed comments.', 
-        max_chars=500
-    )
-    submit_button = st.form_submit_button(label='Submit')
-
-    # If the form is submitted, write the comment to a file
-    if submit_button:
-        try:
-            # Sanitize input
-            sanitized_input = text_input.replace('\n', ' ').strip()
-            if sanitized_input:
-                # Use a relative path to ensure the file is found
-                file_path = os.path.join(comments_dir, 'comments.txt')
-                st.write(f"Debug: Writing to file at {file_path}")
-                with open(file_path, 'a') as f:
-                    f.write(sanitized_input + '\n')
-                st.success("Comment saved successfully!")
-            else:
-                st.warning("Comment cannot be empty.")
-        except FileNotFoundError:
-            st.error("The comments directory or file was not found.")
-        except IOError as e:
-            st.error(f"An I/O error occurred: {e}")
-        except Exception as e:
-            st.error(f"An unexpected error occurred: {e}")
+st.write("To commment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments or to comment directly here is my email stephenmodimakwane@gmail.com")
