5656c93116929d01b334d57de4917d0a7b316b2a
SteeMod <stephenmodimakwane@gmail.com>
2024-08-23 17:55:29 +0000

    It shall work

--- pages/testing.py
+++ pages/testing.py
@@ -1,22 +1,3 @@
 import streamlit as st
 
-# Create a form with a text area in Streamlit
-with st.form(key='Comment'):
-    text_input = st.text_area(
-        label='Enter your comment', 
-        value='', 
-        placeholder='Include who the message is from and give detailed comments.', 
-        max_chars=500
-    )
-    submit_button = st.form_submit_button(label='Submit')
-
-    # If the form is submitted, write the comment to a file
-    if submit_button:
-        try:
-            # Use an absolute path to ensure the file is found
-            with open('comments/comments.txt', 'a') as f:
-                f.write(text_input + '\n')
-            st.success("Comment saved successfully!")
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-            st.error(f"Error details: {str(e)}")
+st.write("To commment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments or to comment directly here is my email stephenmodimakwane@gmail.com")
