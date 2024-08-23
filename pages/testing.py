7a23ba2ad4154856cd039eeaea87dca711e502b7
SteeMod <stephenmodimakwane@gmail.com>
2024-08-23 17:56:14 +0000

    Praise GOD

--- pages/testing.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-
-st.write("To commment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments or to comment directly here is my email stephenmodimakwane@gmail.com")
