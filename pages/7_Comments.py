bdc5657666973ece760ed1ce2efbaba1d3af146f
SteeMod <stephenmodimakwane@gmail.com>
2024-11-30 19:57:59 +0000

    praise the LORD

--- pages/7_Comments.py
+++ pages/7_Comments.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.write("To comment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments .")
-st.write("Alternatively, you can email your comments stephenmodimakwane@gmail.com")
+#st.write("To comment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments .")
+st.write("You can email your comments stephenmodimakwane@gmail.com")
