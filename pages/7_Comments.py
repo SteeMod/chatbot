347ab6bfe10bc267d004f4ff56a849dc05bcda0e
SteeMod <stephenmodimakwane@gmail.com>
2024-12-09 07:48:20 +0000

    GOD is good

--- pages/7_Comments.py
+++ pages/7_Comments.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 #st.write("To comment click on this link, it will redirect you to a different page, https://medtracker.streamlit.app/Comments .")
-st.write("Here is a link to provide feedback on your experience https://www.surveymonkey.com/r/VLSNBRZ")
+st.write("Here is a link to provide feedback on your experience  go to https://www.surveymonkey.com/r/VLSNBRZ")
