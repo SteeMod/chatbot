27e1350e943b650c63477c690ca553cb87d529f8
SteeMod <stephenmodimakwane@gmail.com>
2025-01-06 04:12:57 +0000

    GOD is GOOD

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-st.subheader("Welcome to MYOFree Prototype")
+st.subheader("Welcome to THE Multi Channel Medication Virtual Assistant")
 st.write("This app is designed to  empower invididuals, to be OFree (Opiod Free) and live fufilling lives by providing end to end information on opiod misuse/overdose, the app is still in testing phase so please leave a comment. First to use the app you will need to go to the home page through the sidebar, on mobile it looks like this < login to get started. The username is  myofree, Password: imfree123. Then you may proceed to other pages via the side bar.")
 
 
