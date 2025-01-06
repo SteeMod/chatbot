97aca813c2081de65b4000a8474892751e49c1d9
SteeMod <stephenmodimakwane@gmail.com>
2025-01-06 04:14:55 +0000

    this is the day that the LORD has made I shall be glad and rejoice in it 🎶

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
-st.subheader("Welcome to THE Multi Channel Medication Virtual Assistant")
-st.write("This app is designed to  empower invididuals, to be OFree (Opiod Free) and live fufilling lives by providing end to end information on opiod misuse/overdose, the app is still in testing phase so please leave a comment. First to use the app you will need to go to the home page through the sidebar, on mobile it looks like this < login to get started. The username is  myofree, Password: imfree123. Then you may proceed to other pages via the side bar.")
+st.subheader("Welcome to The Multi Channel Medication Virtual Assistant")
+st.write("This app is designed to  empower invididuals, to be Opiod Free)and live fufilling lives by providing end to end information on opiod misuse/overdose, the app is still in testing phase so please leave a comment. First to use the app you will need to go to the home page through the sidebar, on mobile it looks like this < login to get started. The username is  myofree, Password: imfree123. Then you may proceed to other pages via the side bar.")
 
 
 
@@ -12,9 +12,9 @@ image3 = Image.open("images/image3.png")
 
 # Descriptions of the images
 descriptions = [
-    "Go to the MYOEvent Finder, this page helps you find in person or virtual events (treatment, awareness, Prevention and Support events) about opiod misuse and overdose",
-    "MyOSupport Service Finder lets you find support services (medication, behavioral therapy or helplines) based on your preffered treatment service.",
-    "MyOTP Finder lets you find Opiod Treatment Programs, facilities and healthcare professionals based on your location."
+    "Go to the Event Finder, this page helps you find in person or virtual events (treatment, awareness, Prevention and Support events) about opiod misuse and overdose",
+    "The Support Service Finder lets you find support services (medication, behavioral therapy or helplines) based on your preffered treatment service.",
+    "The OTP Finder lets you find Opiod Treatment Programs, facilities and healthcare professionals based on your location."
 ]
 
 # Display images with descriptions
